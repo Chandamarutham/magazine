612678f9c7c37cad766893fd03bea5a4058acfd8
Balaji Srinivasa <admin@kkcp.chandamarutham.org>
2025-10-16 05:45:41 +0530

    Some corrections in Main Page layout

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -5,7 +5,8 @@ export default function MainPanel() {
     return (
         <div className={styles.mainPanel}>
             <p className={`${styles.preamble} font-mukta`}>ஶ்ரீ:</p>
-            <p className={`${styles.preamble} font-mukta`}>ஶ்ரீமதே ராமாநுஜாய நம: ■ ஶ்ரீஸ்ரீநிவாஸ மஹாகுரவே நம:</p>
+            <p className={`${styles.preamble} font-mukta`}>ஶ்ரீமதே ராமாநுஜாய நம:</p>
+            <p className={`${styles.preamble} font-mukta`}>ஶ்ரீஸ்ரீநிவாஸ மஹாகுரவே நம:</p>
             <img src={bannerImage} className={styles.thirumannImage} alt="திருமண்காப்பு" /> 
         </div>
     );
